0eae61d13fd922777744207a4a6a242be14f550c
teaishealthy <teaishealthy@protonmail.com>
2022-06-19 09:00:54 +0200

    feat: more info and icons on projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://teaishealthy.me"
+  site: "https://teaishealthy.me",
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
