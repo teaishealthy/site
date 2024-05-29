f5e696f9bd36d8c346bd1fe54e6f9e91c3142afd
teaishealthy <teaishealthy@protonmail.com>
2024-05-29 21:57:59 +0200

    build: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://teaishealthy.me",
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
-  integrations: [sitemap()],
+
+  integrations: [sitemap(), icon()],
 });
