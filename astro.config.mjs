9e12e710f2d873411ccb5b3c5fdda91707992e61
teaishealthy <teaishealthy@protonmail.com>
2024-05-29 22:47:28 +0200

    refactor: integrate tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://teaishealthy.me",
-
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap(), icon(), tailwind()],
 });
